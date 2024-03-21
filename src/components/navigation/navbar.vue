6f643b07ac54cda5daf7ebe540d66d2cbbaa43e3
Dmitriy Vintsyk <Dmitriy.Vintsyk@devoxsoftware.com>
2024-03-21 17:19:07 +0200

    Fix layout

--- src/components/navigation/navbar.vue
+++ src/components/navigation/navbar.vue
@@ -5,29 +5,27 @@ const loading = ref(false);
 
 </script>
 <template>
-  <div class="px-8 py-5">
-    <div class="max-w-screen-2xl mx-auto">
-      <v-toolbar class="bg-white text-black">
-        <router-link class="w-1/5 px-5" to="/">
-          <v-img src="src/assets/logo-black.svg"></v-img>
-        </router-link>
-        <v-text-field
-          class="mx-10"
-          append-inner-icon="mdi-magnify"
-          density="compact"
-          label="Пошук по VIN"
-          variant="solo"
-          hide-details
-          single-line
-          @click:append-inner="() => loading = !loading"
-        ></v-text-field>
-        <v-btn icon>
-          <v-icon size="large">mdi-account</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-cart</v-icon>
-        </v-btn>
-      </v-toolbar>
+  <v-toolbar class="bg-white text-black" height="100">
+    <div class="flex max-w-screen-2xl mx-auto px-8 w-full">
+      <router-link class="w-1/5 px-5" to="/">
+        <v-img src="src/assets/logo-black.svg"></v-img>
+      </router-link>
+      <v-text-field
+        class="mx-10"
+        append-inner-icon="mdi-magnify"
+        density="compact"
+        label="Пошук по VIN"
+        variant="solo"
+        hide-details
+        single-line
+        @click:append-inner="() => loading = !loading"
+      ></v-text-field>
+      <v-btn icon>
+        <v-icon size="large">mdi-account</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
     </div>
-  </div>
+  </v-toolbar>
 </template>
--- src/components/navigation/subnavbar.vue
+++ src/components/navigation/subnavbar.vue
@@ -1,22 +1,13 @@
 <script setup>
 </script>
 <template>
-    <v-toolbar class="bg-grey-darken-4 text-white">
-    <div class="max-w-screen-2xl mx-auto w-full">
-          <v-btn icon>
-            <v-icon size="large">mdi-account</v-icon>
-          </v-btn>
-          <v-btn icon>
-            <v-icon size="large">mdi-account</v-icon>
-          </v-btn>
-        </div>
-    </v-toolbar>
-
-    <!-- <nav class="container">
-      <div class="flex">
-        <router-link to="/">
-          Logo
-        </router-link>
-      </div>
-    </nav> -->
+  <v-toolbar class="bg-grey-darken-4 text-white">
+    <div class="max-w-screen-2xl mx-auto w-full px-8">
+      <v-btn>
+        <v-app-bar-nav-icon/>
+        <v-title>Категории</v-title>
+      </v-btn>
+      
+    </div>
+  </v-toolbar>
 </template>
