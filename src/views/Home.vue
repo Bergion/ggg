f530b9c0209440c2cc5d8330046a932884c66514
Dmitriy Vintsyk <dmitriy.vintsyk@devoxsoftware.com>
2024-03-25 16:06:23 +0200

    Visual

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,37 @@
 </script>
 
 <template>
-	<v-container fluid>
-		<v-row class="max-w-screen-2xl mx-auto w-full px-8">
-			<div> zhopa </div>
+	<v-container class="px-4" fluid>
+		<v-row class="max-w-screen-2xl mx-auto w-full">
+			<v-col sm="8" cols="12">
+				<v-card
+					class="mx-auto"
+				>
+					<v-carousel
+						height="400"
+						show-arrows="hover"
+						cycle
+						hide-delimiter-background
+					>
+						<v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+						</v-carousel-item>
+					</v-carousel>
+				</v-card>
+			</v-col> 
+			<v-col sm="4">
+				<v-card
+					class="mx-auto"
+					prepend-icon="mdi-home"
+				>
+					<template v-slot:title>
+						This is a title
+					</template>
+
+					<v-card-text>
+						This is content
+					</v-card-text>
+				</v-card>
+			</v-col> 
 		</v-row>
 	</v-container>
 </template>
